a0d1cfe023a9f87d1d3b8774ce906ffc6288f383
Luis Carlin <luiscarlin@gmail.com>
2019-06-19 05:59:54 -0400

    organize index and app better

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-function Root() {
-  return (
-    <div>
-      <h1>Hello from react</h1>
-    </div>
-  );
-}
-
-let App = document.getElementById("app");
-
-ReactDOM.render(<Root />, App);
+ReactDOM.render(<App />, document.getElementById("app"));
