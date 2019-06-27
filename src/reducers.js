1108918b994aa984479045ed2d07d93a3de855e2
Luis Carlin <luiscarlin@gmail.com>
2019-06-27 07:20:16 -0400

    start setting up

---
+++ src/reducers.js
@@ -0,0 +1,25 @@
+const initialState = {
+  episodes: [],
+  favourites: []
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    case "ADD_FAV":
+      return {
+        ...state,
+        favourites: [...state.favourites, action.payload]
+      };
+    case "REMOVE_FAV":
+      return {
+        ...state,
+        favourites: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
