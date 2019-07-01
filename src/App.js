0f48fb86249e18fdfd1a574d8e946b9eebf7481c
Luis Carlin <luiscarlin@gmail.com>
2019-07-01 07:08:53 -0400

    todo app to practice hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { timeout } from "q";
+
+function appReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: "",
+          completed: false
+        }
+      ];
+
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(appReducer, []);
   return (
     <div>
-      <h1>Hello from react app</h1>
+      <h1>Todo App</h1>
+      <button onClick={() => dispatch({ type: "add" })}>Add</button>
+      {state.map(item => (
+        <TodosItem key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+
+function TodosItem({ id, completed, text }) {
+  return (
+    <div>
+      <input type="checkbox" />
+      <input type="text" defaultValue={text} />
+      <button>delete</button>
     </div>
   );
 }
