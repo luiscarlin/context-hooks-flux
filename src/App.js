3a90647f9ec278c1e9bc9912d31353cddf442ed7
Luis Carlin <luiscarlin@gmail.com>
2019-07-01 07:46:21 -0400

    working on updtae

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useReducer } from "react";
-import { timeout } from "q";
+import React, { useReducer, useContext } from "react";
 
 function appReducer(state, action) {
   switch (action.type) {
@@ -12,30 +11,49 @@ function appReducer(state, action) {
           completed: false
         }
       ];
-
+    case "update":
+      return state.map(item => {
+        if (item.id === action.payload.id) {
+          console.log({ ...item, ...action.payload });
+          return { ...item, ...action.payload };
+        }
+        return item;
+      });
     default:
       return state;
   }
 }
 
+const Context = React.createContext();
+
 function App() {
   const [state, dispatch] = useReducer(appReducer, []);
   return (
-    <div>
+    <Context.Provider value={dispatch}>
       <h1>Todo App</h1>
       <button onClick={() => dispatch({ type: "add" })}>Add</button>
       {state.map(item => (
         <TodosItem key={item.id} {...item} />
       ))}
-    </div>
+    </Context.Provider>
   );
 }
 
 function TodosItem({ id, completed, text }) {
+  const dispatch = useContext(Context);
   return (
     <div>
       <input type="checkbox" />
-      <input type="text" defaultValue={text} />
+      <input
+        type="text"
+        value={text}
+        onChange={event =>
+          dispatch({
+            type: "update",
+            payload: { id: id, text: event.target.value }
+          })
+        }
+      />
       <button>delete</button>
     </div>
   );
