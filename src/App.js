0f2c8e539887ac85e7ba92f7715c39e67fe1e8e8
Luis Carlin <luiscarlin@gmail.com>
2019-07-03 07:52:26 -0400

    fixed some formatting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div>
-      <h1>TUDU</h1>
+      <h1>TUDUs</h1>
       <button onClick={() => dispatch(actions.add())}>Add</button>
       {state.map(item => (
         <TodosItem key={item.id} {...item} />
