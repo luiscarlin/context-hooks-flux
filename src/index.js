a9d6931acb9450e4084b9eb8a9e4eb067c6da248
Luis Carlin <luiscarlin@gmail.com>
2019-07-04 21:41:14 -0400

    add scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppState } from "./context";
+import "./index.scss";
 
 function Root() {
   return (
