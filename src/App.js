ca21164d0b2b5491c5f97a56d5e5b849b43ca808
Luis Carlin <luiscarlin@gmail.com>
2019-07-08 07:32:29 -0400

    get tests running

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { useContext } from "react";
 import { AppContext } from "./context";
 import TodosItem from "./TodosItem";
 import actions from "./actions";
-import "./app.scss";
 
 function App() {
-  const { state, dispatch } = useContext(AppContext);
+  // const { state, dispatch } = useContext(AppContext);
 
   return (
     <main className="app">
       <h1>Do this</h1>
-      <button onClick={() => dispatch(actions.add())}>Add</button>
+      {/* <button onClick={() => dispatch(actions.add())}>Add</button>
       {state.map(item => (
         <TodosItem key={item.id} {...item} />
-      ))}
+      ))} */}
     </main>
   );
 }
