8d3d301210d1f8145b2aed31343f80fd36abd11b
Luis Carlin <luiscarlin@gmail.com>
2019-07-09 07:16:35 -0400

    test date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import moment from "moment";
 
 function Header() {
   return (
     <header>
       <h1 data-testid="header__title">Do this</h1>
+      <h2 data-testid="header__date">{moment().format("dddd, MMMM Do")}</h2>
     </header>
   );
 }
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,12 +2,27 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import Header from "./Header";
 import "@testing-library/jest-dom/extend-expect";
+import moment from "moment";
 
 afterEach(cleanup);
 
+jest.mock("moment", () => () => ({
+  format: () => "Monday, July 8th"
+}));
+
 describe("Header", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render(<Header />);
+  });
   it("renders the title", () => {
-    const wrapper = render(<Header />);
     expect(wrapper.getByTestId("header__title")).toHaveTextContent("Do this");
+  });
+
+  it("renders the current date", () => {
+    expect(wrapper.getByTestId("header__date")).toHaveTextContent(
+      "Monday, July 8th"
+    );
   });
 });
