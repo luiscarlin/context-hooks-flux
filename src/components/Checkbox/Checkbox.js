0cc1ebbde64908cfa4f36192116bba6c8f03dc85
Luis Carlin <luiscarlin@gmail.com>
2019-07-15 07:44:25 -0400

    add checkbox

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./checkbox.scss";
+
+function Checkbox({ isChecked, onToggle }) {
+  return <div>hello</div>;
+}
+
+export default Checkbox;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,25 +2,28 @@ import React from "react";
 import "./todo.scss";
 import CloseIcon from "./CloseIcon";
 
+const PLACEHOLDER_TEXT = "What do you want to do?....";
+
 function Todo({ text, onTextUpdate, isCompleted, onDelete, onToggle }) {
   return (
-    <>
-      <input
-        data-testid="checkbox"
-        type="checkbox"
-        checked={isCompleted}
-        onChange={onToggle}
-      />
+    <div className="todo">
+      <div className="todo__checkbox">{isCompleted && "C"}</div>
       <input
         data-testid="input-text"
+        className="todo__task"
         type="text"
+        placeholder={PLACEHOLDER_TEXT}
         onChange={event => onTextUpdate(event.target.value)}
         value={text}
       />
-      <button data-testid="delete-button" onClick={onDelete}>
+      <button
+        data-testid="delete-button"
+        className="todo__delete"
+        onClick={onDelete}
+      >
         <CloseIcon />
       </button>
-    </>
+    </div>
   );
 }
 
